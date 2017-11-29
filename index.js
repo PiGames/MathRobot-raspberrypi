09c3f379d1ea1551d36495933af5d57b9d0b1f28
pietrzakacper <kacpietrzak@gmail.com>
2017-11-29 17:19:07 +0100

    Yollo

---
+++ index.js
@@ -0,0 +1,53 @@
+const backend = require('socket.io-client').connect('https://mathrobot.herokuapp.com/')
+const app = require('express')();
+const http = require('http').Server(app);
+const socket = require('socket.io')(http);
+
+let equation = []
+let currentProcessedCharacterIndex = -1
+
+backend.on('connect', () => {
+  console.log('connected to the server')
+});
+
+backend.on('equation incoming', eq => {
+	if(equation) {
+		backend.emit('error', 'Robot is already processing one equation')
+		return
+	}
+
+	equation = eq
+	startProccesingEquation()
+})
+
+const eqationMap = require('./buttonsLocationMap.js')
+
+let arduino;
+
+io.on('connection', socket =>{
+  arduino = socket
+
+  arduino.on('clicked', ()=>{
+    clickNext(arduino)
+  })
+
+  arduino.on('end', resultImg => {
+    backend.emit('equation calculated', resultImg)
+  })
+})
+
+
+function startProccesingEquation() {
+  if(!arduino) {
+    console.log('arduino not connected')
+    return;
+  }
+
+  currentProcessedCharacterIndex = 0;
+  arduino.emit('click', eqationMap[equation[0]])
+}
+
+
+function clickNext() {
+  arduino.emit('click', eqationMap[equation[++currentProcessedCharacterIndex]])
+}
