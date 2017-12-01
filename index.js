7df70a707f4abaf57ff2f59fd08437a2033bf8d3
Kacper Pietrzak <pietrk01@stepstone.com>
2017-12-01 23:43:07 +0100

    gggd

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const backend = require('socket.io-client').connect('https://mathrobot.herokuapp.com/')
+const backend = require('socket.io-client').connect('http://localhost:4200', {path:'/raspberry'})
 const app = require('express')();
 const http = require('http').Server(app);
-const socket = require('socket.io')(http);
+const io = require('socket.io')(http);
 const NodeWebcam = require( "node-webcam" );
 const getDate = () => {
   const d = new Date
   return `${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}_${d.getDate()}-${d.getMonth()}-${d.getFullYear()}`
 }
 
-let equation = []
+let equation
 let currentProcessedCharacterIndex = -1
 
-backend.on('connect', () => {
-  console.log('connected to the server')
-});
+backend.on('connect', () => {console.log('connected')});
 
 backend.on('equation incoming', eq => {
+  console.log(eq, 'just got equation')
+  
 	if(equation) {
 		backend.emit('error', 'Robot is already processing one equation')
 		return
 	}
-
-	equation = eq
+	equation = []
 	startProccesingEquation()
 })
 
@@ -34,18 +33,34 @@ io.on('connection', socket =>{
   arduino = socket
 
   arduino.on('clicked', ()=>{
-    clickNext(arduino)
-  })
+    // TODO uncomment and delete the copy from startProcessingEquation after setting up arduino
 
-  arduino.on('end', resultImg => {
-      NodeWebcam.capture( `imgs/${getDate()}`, {callbackReturn: "base64"}, ( err, data ) =>
-        backend.emit('equation calculated', resultImg)
-      );
+    // if(currentProcessedCharacterIndex === equation.length - 1){
+    //   NodeWebcam.capture( `imgs/${getDate()}`, {callbackReturn: "base64", sleep: 20}, ( err, data ) =>{
+    //   if(err) {
+    //     console.log(err)
+    //     return
+    //   }
+    //   backend.emit('equation calculated', data)
+    // });
+    // }
+    clickNext(arduino)
   })
 })
 
 
 function startProccesingEquation() {
+  if(currentProcessedCharacterIndex === equation.length - 1){
+    console.log('take a pic')
+    NodeWebcam.capture( `imgs/${getDate()}`, {callbackReturn: "base64", sleep: 20}, ( err, data ) =>{
+    if(err) {
+      console.log(err)
+      return
+    }
+    console.log('pic sent ')
+    backend.emit('equation calculated', data)
+  });
+  }
   if(!arduino) {
     console.log('arduino not connected')
     return;
